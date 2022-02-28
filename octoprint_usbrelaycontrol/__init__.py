2a0ed070f393555f826a8494b744e0e01835c257
A. S. Budden <abudden@gmail.com>
2022-02-28 18:26:37 +0000

    Added permissions registration thing as per https://github.com/OctoPrint/plugins.octoprint.org/pull/1014#issuecomment-1054538681

--- octoprint_usbrelaycontrol/__init__.py
+++ octoprint_usbrelaycontrol/__init__.py
@@ -214,5 +214,6 @@ def __plugin_load__():
 
     global __plugin_hooks__
     __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.access.permissions": __plugin_implementation__.get_additional_permissions,
     }
